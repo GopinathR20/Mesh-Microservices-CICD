126798ed25cbf111c37bb9c8bae6ccc74c896a1a
Gopinath <rajagokul231@gmail.com>
2025-10-29 19:46:17 +0530

    Final code for Web App deployment with single pipeline 2

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -31,7 +31,8 @@ resource "azurerm_cosmosdb_account" "cosmos" {
   kind                = var.cosmos_db_kind
 
   consistency_policy { consistency_level = "Session" }
-  geo_location { location = azurerm_resource_group.rg.location; failover_priority = 0 }
+  geo_location { location = azurerm_resource_group.rg.location
+                  failover_priority = 0 }
 }
 resource "random_integer" "suffix" { min = 10000; max = 99999 }
 
