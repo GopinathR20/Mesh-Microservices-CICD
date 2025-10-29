8b4379eae148f2c9590d99f5dd317473aedf317c
Gopinath <rajagokul231@gmail.com>
2025-10-29 20:32:23 +0530

    changed yml filename

--- Mesh-Microservices/user-service/src/main/java/com/mesh_microservices/user_service/UserServiceApplication.java
+++ Mesh-Microservices/user-service/src/main/java/com/mesh_microservices/user_service/UserServiceApplication.java
@@ -12,7 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class UserServiceApplication {
-
+	//Trigger pipeline run
 	/**
 	 * The main method that launches the User Service application.
 	 *
