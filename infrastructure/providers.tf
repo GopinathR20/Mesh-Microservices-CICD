af316cf3aade85e15f5d2f2188986e70607a6263
Gopinath <rajagokul231@gmail.com>
2025-10-29 23:07:08 +0530

    changed main.tf

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -2,12 +2,7 @@
 
 terraform {
   required_version = ">= 1.0"
-
   required_providers {
-    azuredevops = {
-      source  = "microsoft/azuredevops"
-      version = ">= 0.4.0"
-    }
     azurerm = {
       source  = "hashicorp/azurerm"
       version = ">= 3.74.0"
@@ -16,13 +11,32 @@ terraform {
       source  = "hashicorp/random"
       version = "~> 3.0"
     }
+    # Add azuredevops provider if you are creating pipelines
+    azuredevops = {
+      source  = "microsoft/azuredevops"
+      version = ">= 0.4.0"
+    }
   }
-}
-
-provider "azuredevops" {
-  org_service_url = var.azdo_org_service_url
 }
 
 provider "azurerm" {
   features {}
+}
+
+# Add this provider block if you are creating pipelines
+provider "azuredevops" {
+  org_service_url = var.azdo_org_service_url
+}
+
+# This block tells Terraform to use your existing Cloud Shell storage
+terraform {
+  backend "azurerm" {
+    # This is the resource group for your Cloud Shell storage
+    resource_group_name  = "cloud-shell-storage-centralindia"
+    # This is the storage account name from your screenshot
+    storage_account_name = "csg100320035b718cb1"
+    # This container already exists
+    container_name       = "tfstate"
+    key                  = "mesh.prod.terraform.tfstate"
+  }
 }
