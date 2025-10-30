3beb46cc90443df5ff30971a082bb76888b822d7
Gopinath <rajagokul231@gmail.com>
2025-10-30 14:23:48 +0530

    co2

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -25,10 +25,6 @@ terraform {
   }
 }
 
-# ✅ Azure DevOps Provider
-provider "azuredevops" {
-  org_service_url = var.azdo_org_service_url
-}
 
 # ✅ Azure Provider — Explicitly use pipeline SPN credentials
 provider "azurerm" {
