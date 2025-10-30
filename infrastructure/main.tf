db4f4d20a957144df60b01147b6b4f7736f18973
Gopinath <rajagokul231@gmail.com>
2025-10-30 19:27:32 +0530

    change in resourcegroup

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,5 +1,11 @@
+resource "random_string" "suffix" {
+  length  = 4
+  special = false
+  upper   = false
+}
+
 resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
+  name     = "${var.resource_group_name}-${random_string.suffix.result}"
   location = var.location
 }
 
