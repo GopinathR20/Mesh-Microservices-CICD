6f00404233f8a4dff7f5beabcfcba7bc9392d0bd
Gopinath <rajagokul231@gmail.com>
2025-10-30 14:54:03 +0530

    providers change for remove devops

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -2,21 +2,19 @@ terraform {
   required_version = ">= 1.0"
 
   required_providers {
-    azuredevops = {
-      source  = "microsoft/azuredevops"
-      version = ">= 0.4.0"
-    }
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 3.74.0"
-    }
-    random = {
-      source  = "hashicorp/random"
+      source = "hashicorp/azurerm"
       version = "~> 3.0"
     }
+    random = {
+      source = "hashicorp/random"
+      version = ">= 3.0"
+    }
+    ad = {
+      source  = "hashicorp/ad"
+      version = "0.5.0"
+    }
   }
-
-  # ✅ Backend block (Terraform remote state)
   backend "azurerm" {
     resource_group_name  = "cloud-shell-storage-centralindia"
     storage_account_name = "csg100320035b718cb1"
@@ -25,8 +23,7 @@ terraform {
   }
 }
 
-
-# ✅ Azure Provider — Explicitly use pipeline SPN credentials
 provider "azurerm" {
-  features {}
+  features{}
+
 }
